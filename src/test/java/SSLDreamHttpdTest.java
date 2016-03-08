fb23a02d19fd5bb77b57813bdf54427ffdc729d8
forDream <gam2046@gmail.com>
2016-03-08 16:30:43 +0800

    [fix]test module
    add test jks

--- src/test/java/SSLDreamHttpdTest.java
+++ src/test/java/SSLDreamHttpdTest.java
@@ -1,21 +1,29 @@
 import fordream.http.SSLDreamHttpd;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Random;
 
 /**
  * Created by forDream on 2016-03-07.
  */
 public class SSLDreamHttpdTest {
     private SSLDreamHttpd httpd;
+    private int httpPort;
+    private int httpsPort;
 
     @Before
     public void startup() throws IOException {
-        httpd = new SSLDreamHttpd(80, 443, "/keystore.jks", "4357211", null);
+        File trustStore = new File("src/test/resources/keystore.jks");
+        System.setProperty("javax.net.ssl.trustStore", trustStore.getAbsolutePath());
+        Random random = new Random();
+        this.httpPort = random.nextInt(1024) + 2048;
+        this.httpsPort = random.nextInt(1024) + 4096;
+        httpd = new SSLDreamHttpd(this.httpPort, this.httpsPort, "/" + trustStore.getName(), "password", null);
         httpd.registerHandler(new TestRequetHandler());
         httpd.start();
     }
@@ -27,13 +35,13 @@ public class SSLDreamHttpdTest {
 
     @Test
     public void httpTest() throws IOException {
-        HttpRequester.HttpResponse response = HttpRequester.doRequest(new URL("http://127.0.0.1"), "GET", null, null, false);
+        HttpRequester.HttpResponse response = HttpRequester.doRequest(new URL("http://127.0.0.1:" + this.httpPort), "GET", null, null, false);
         assert response.getResponseCode() == 200;
     }
 
     @Test
     public void httpsTest() throws IOException {
-        HttpRequester.HttpResponse response = HttpRequester.doRequest(new URL("https://127.0.0.1"), "GET", null, null, false);
+        HttpRequester.HttpResponse response = HttpRequester.doRequest(new URL("https://127.0.0.1:" + this.httpsPort), "GET", null, null, false);
         assert response.getResponseCode() == 200;
     }
 }
